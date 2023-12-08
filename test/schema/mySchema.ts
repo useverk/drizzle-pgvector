db1608fbf9dbee177b44bf3469f1e2f65761b01e
Simon Sardorf <ssardorf@gmail.com>
2023-12-08 12:13:18 +0100

    Remove test folder

--- test/schema/mySchema.ts
+++
@@ -1,7 +0,0 @@
-import { pgTable, uuid } from "drizzle-orm/pg-core";
-import { customVector } from "../..";
-
-export const myTable = pgTable("jobSeekerProfile", {
-    id: uuid("id").notNull().primaryKey().defaultRandom(),
-    embedding: customVector("embedding", { dimensions: 1536 }).notNull(),
-});
